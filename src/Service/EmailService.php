e3f66056e4c70cb326eafd064a71846f5de6d3b4
jihaneha <ghamidou800@gmail.com>
2023-08-23 13:13:41 +0200

    feat/ notifications

---
+++ src/Service/EmailService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class EmailService
+{
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendNotificationEmail(string $recipient, string $subject, string $content)
+    {
+        $email = (new Email())
+            ->from('ghamidou800@gmail.com')
+            ->to($recipient)
+            ->subject($subject)
+            ->html($content);
+
+        $this->mailer->send($email);
+    }
+}
